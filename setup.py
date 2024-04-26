c1d3c7de27cb51f8b4a82a70130ec9df1b1e9072
iffthomas <thomasiff@outlook.de>
2024-04-26 20:31:20 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Our submission for the Datathon 2024 offered from the Analytics club',
+    author='Andreas Frey, Cedric Keller, Clemens Keusch, Thomas Iff',
+    license='MIT',
+)
